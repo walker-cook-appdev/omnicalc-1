797608f10385a88b17d34c228831796684f26b46
walker-cook <jwcook99@gmail.com>
2022-01-26 15:44:39 +0000

    First

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+    render({:template => "calculation_templates/square_form.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 
+get("/square/new",{:controller => "application", :action => "blank_square_form"})
 end
