858133d14a0435fa3754fb08eff7c1302ae2cdad
walker-cook <jwcook99@gmail.com>
2022-01-26 15:59:14 +0000

    Square with form completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   def blank_square_form
     render({:template => "calculation_templates/square_form.html.erb"})
   end
+
+  def calculate_square
+    @num = params.fetch("number").to_f
+    @square_of_num = @num ** 2
+    render({:template => "calculation_templates/square_results.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
 get("/square/new",{:controller => "application", :action => "blank_square_form"})
+get("/square/results",{:controller => "application", :action => "calculate_square"})
 end
